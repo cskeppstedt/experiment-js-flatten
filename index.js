87803984840bc4474f29dbec359fd0022f78b187
Christoffer Skeppstedt <chris.skeppstedt@klarna.com>
2017-02-19 18:01:34 +0100

    Add implementation

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-module.exports = (input) => {
+module.exports = function flatten (input) {
+  if (!isArray(input)) {
+    throw new TypeError('`input` must be an array')
+  }
+
+  return flattenArray(input)
+}
+
+function flattenArray (input) {
+  return input.reduce((accumulator, element) => (
+    isArray(element)
+      ? [...accumulator, ...flattenArray(element)]
+      : [...accumulator, element]
+  ), [])
+}
+
+function isArray (input) {
+  return input.constructor === Array
 }
 
--- index.test.js
+++ index.test.js
@@ -1,3 +1,39 @@
 const flatten = require('./')
 
-test('')
+test('throws on invalid input', () => {
+  const invalidInput = [undefined, null, 3, '3', {}, false, true]
+  invalidInput.forEach((input) => {
+    expect(() => flatten(input)).toThrow(TypeError)
+  })
+})
+
+test('handles empty array', () => {
+  const input = []
+  const expected = []
+  expect(flatten(input)).toEqual(expected)
+})
+
+test('handles single item array', () => {
+  const input = [1]
+  const expected = [1]
+  expect(flatten(input)).toEqual(expected)
+})
+
+test('handles multiple item array', () => {
+  const input = [1, 2, 3]
+  const expected = [1, 2, 3]
+  expect(flatten(input)).toEqual(expected)
+})
+
+test('handles single level nesting', () => {
+  const input = [1, [2, 3], 4]
+  const expected = [1, 2, 3, 4]
+  expect(flatten(input)).toEqual(expected)
+})
+
+test('handles multiple level nesting', () => {
+  const input = [1, [2, [3, 4]], 5]
+  const expected = [1, 2, 3, 4, 5]
+  expect(flatten(input)).toEqual(expected)
+})
+
